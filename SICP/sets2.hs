e642f8470981a4559e8eb5c2762aeba87a42fc33
Meowcolm024 <luoyicong1026@live.com>
2020-08-27 19:29:27 +0800

    tree

--- SICP/sets2.hs
+++ SICP/sets2.hs
@@ -23,3 +23,15 @@ e1 = [1..5]
 e2 :: [Integer]
 e2 = [1..9] ++ [13..18]
 
+data Tree a = Node a (Tree a) (Tree a) | Empty deriving (Show, Eq)
+
+eleTree :: Ord a => a -> Tree a -> Bool
+eleTree _ Empty = False
+eleTree x (Node y left right) 
+  | x == y = True
+  | x < y = eleTree x left
+  | x > y = eleTree x right
+
+-- >>> eleTree 3 $ Node 5 (Node 3 Empty Empty) (Node 5 Empty Empty)-- attempting to use module ‘fake_uid:Main’ (/Users/yatchunglaw/Playground/SICP/sets2.hs) which is not loaded
+-- True
+--
