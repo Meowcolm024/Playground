47e800afcdd13e3048a8dfc2fa5e49be1b4c625c
Meowcolm024 <luoyicong1026@live.com>
2020-09-13 22:42:18 +0800

    simp

--- C++/my_if.cpp
+++ C++/my_if.cpp
@@ -1,29 +1,25 @@
 #include <iostream>
 using namespace std;
 
-function<int(int)> my_true(int x)
+int my_true(int x, int y)
 {
-    return [x](int y) { return x; };
+    return x;
 }
 
-function<int(int)> my_false(int x)
+int my_false(int x, int y)
 {
-    return [x](int y) { return y; };
+    return y;
 }
 
-function<function<int(int)>(int)> my_if(function<function<int(int)>(int)> p)
+int my_if(function<int(int, int)> p, int a, int b)
 {
-    return [p](int a) {
-        return [p, a](int b) {
-            return p(a)(b);
-        };
-    };
+    return p(a,b);
 }
 
 int main()
 {
-    int x = my_if(my_true)(1)(2);
-    int y = my_if(my_false)(1)(2);
+    int x = my_if(my_true, 1, 2);
+    int y = my_if(my_false, 1, 2);
     cout << x << " " << y << endl;
     return 0;
 }
