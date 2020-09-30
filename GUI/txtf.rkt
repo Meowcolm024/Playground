fe4adf907d8fe0c8a8bf36a09407c41b4b62f769
Meowcolm024 <luoyicong1026@live.com>
2020-09-30 18:07:34 +0800

    uiui

---
+++ GUI/txtf.rkt
@@ -0,0 +1,40 @@
+#lang racket/gui
+
+(define app (new frame%
+                   [label "Example"]
+                   [width 400]
+                   [height 300]))
+
+(define panel (new horizontal-panel% [parent app]
+                                     [alignment '(center center)]))
+
+(define editor-canvas (new editor-canvas%
+                           (parent panel)
+                           (label "Editor Canvas")))
+(define text (new text%))
+(send text insert "Editor Canvas\n")
+(send editor-canvas set-editor text)
+
+(define btn-panel (new vertical-panel% [parent panel]
+                                       [alignment '(center center)]))
+
+(define text-field (new text-field%
+                        (label "Text")
+                        (parent btn-panel)
+                        (init-value "Field")))
+
+
+(define btn-one (new button%
+                    (parent btn-panel)
+                    (label "Add text")
+                    [callback (lambda (button event)
+                         (send text insert (string-append (send text-field get-value) "\n")))]))
+
+(define btn-two (new button%
+                    (parent btn-panel)
+                    (label "Clean")
+                    [callback (lambda (button event)
+                         (send text erase))]))
+
+
+(send app show #t)
