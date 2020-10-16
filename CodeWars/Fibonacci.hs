526d2cc9eb7ee97fa3a46674c2883d7eff07043d
Meowcolm024 <luoyicong1026@live.com>
2020-10-16 21:13:13 +0800

    omg

--- CodeWars/Fibonacci.hs
+++ CodeWars/Fibonacci.hs
@@ -3,7 +3,6 @@ module Fibonacci where
 fib :: Integer -> Integer
 fib n = let i = abs n in if n >= 0 then fib'' i else (-1) ^ (i -1) * (fib'' i)
 
--- This works
 fib'' :: Integer -> Integer
 fib'' = iter 1 0 0 1
   where
@@ -13,7 +12,7 @@ fib'' = iter 1 0 0 1
         then iter a b (p * p + q * q) (q * q + 2 * p * q) (n `quot` 2)
         else iter (b * q + a * q + a * p) (b * p + a * q) p q (n -1)
 
--- This fails
+-- 50 times slower
 fib' :: Integer -> Integer
 fib' 0 = 0
 fib' 1 = 1
@@ -21,8 +20,8 @@ fib' n =
   if even n
     then
       let i = n `quot` 2
-          j = fib i
+          j = fib' i
        in (2 * fib' (i -1) + j) * j
     else
       let i = (n + 1) `quot` 2
-       in (fib' i) ^ 2 + (fib (i -1)) ^ 2
+       in (fib' i) ^ 2 + (fib' (i -1)) ^ 2
