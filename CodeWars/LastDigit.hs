409df5a16326556cccc86609e736dd8bcf5a2cfc
Meowcolm024 <luoyicong1026@live.com>
2020-11-12 22:48:05 +0800

    orz

--- CodeWars/LastDigit.hs
+++ CodeWars/LastDigit.hs
@@ -1,18 +1,13 @@
 module LastDigit (lastDigit) where
 
 lastDigit :: [Integer] -> Integer
-lastDigit [] = 1
-lastDigit (1 : _) = 1
-lastDigit as = (`rem` 10) $ powerRem as
-
-powerRem :: [Integer] -> Integer
-powerRem = go
+lastDigit = (`rem` 10) . go
   where
     go [] = 1
     go [x] = x
     go (x : y : r)
       | odd x && odd y = x ^ (go (y : r) `rem` (x + 1))
-      | odd x && even y = x ^ (foldMod y (powerRem r) (x + 1))
+      | odd x && even y = x ^ (foldMod y (go r) (x + 1))
       | otherwise = undefined -- ! failed
 
 foldMod :: Integer -> Integer -> Integer -> Integer
