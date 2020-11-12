2b5ce4491c07ef8388037430b0ab0e0a8709022d
Meowcolm024 <luoyicong1026@live.com>
2020-11-12 22:38:35 +0800

    qaq

--- CodeWars/LastDigit.hs
+++ CodeWars/LastDigit.hs
@@ -13,39 +13,9 @@ powerRem = go
     go (x : y : r)
       | odd x && odd y = x ^ (go (y : r) `rem` (x + 1))
       | odd x && even y = x ^ (foldMod y (powerRem r) (x + 1))
-      | otherwise = ldPower (ld' x) $ powerRem (y : r) -- ! failed
+      | otherwise = undefined -- ! failed
 
 foldMod :: Integer -> Integer -> Integer -> Integer
 foldMod _ 0 _ = 1
 foldMod base 1 modulo = base `rem` modulo
 foldMod base power modulo = (base * foldMod base (power -1) modulo) `rem` modulo
-
-ldPower :: Integer -> Integer -> Integer
-ldPower 0 x = if even (ld x 0) then 1 else 0
-ldPower 2 x = [1, 2, 4, 8, 6] !! (ld x 2)
-ldPower 4 x = [1, 4, 6] !! (ld x 4)
-ldPower 6 x = [1, 6, 6] !! (ld x 6)
-ldPower 8 x = [1, 8, 4, 2, 6] !! (ld x 8)
-ldPower 10 _ = 10
-ldPower _ _ = error "???"
-
-ld :: Integer -> Int -> Int
-ld n m =
-    if x == "0"
-       then 0
-       else let p = read x `rem` g m in if p == 0 then g m else p
-  where 
-      x = reverse . take 2 . reverse $ show n
-      g 8 = 4
-      g 6 = 2
-      g 4 = 2
-      g 2 = 4
-      g 0 = 2
-      g _ = undefined
-
-ld' :: Integer -> Integer
-ld' n =
-  let x = reverse . take 2 . reverse $ show n
-   in if x == "0"
-       then 0
-       else read x `rem` 10
