51bc031e382c5362bde4ed3a16c1ac43d143aacc
Meowcolm024 <luoyicong1026@live.com>
2021-01-07 12:25:39 +0800

    update

--- Haskell3/BalanceTree.hs
+++ Haskell3/BalanceTree.hs
@@ -13,12 +13,12 @@ data Color = Red | Black deriving (Show)
 data Tree a
   = Leaf
   | Node
-      { key :: Int,
-        val :: a,
-        left :: Tree a,
-        right :: Tree a,
-        height :: Int,
-        color :: Color
+      { _key :: Int,
+        _val :: a,
+        _left :: Tree a,
+        _right :: Tree a,
+        _size :: Int,
+        _color :: Color
       }
 
 instance Show a => Show (Tree a) where
@@ -26,9 +26,9 @@ instance Show a => Show (Tree a) where
     show (Node k _ l r _ c) =
         show l ++ " | (" ++ show k ++ "-" ++ show c ++ ") | " ++ show r
 
-setColor :: Tree a -> Color -> Tree a
-setColor Leaf               _ = Leaf
-setColor (Node k v l r s _) c = Node k v l r s c
+setColor :: Color -> Tree a -> Tree a
+setColor _ Leaf               = Leaf
+setColor c (Node k v l r s _) = Node k v l r s c
 
 isRed :: Tree a -> Bool
 isRed (Node _ _ _ _ _ Red) = True
@@ -44,15 +44,15 @@ setRight (Node k v l _ s c) r = Node k v l r s c
 
 size :: Tree a -> Int
 size Leaf     = 0
-size n@Node{} = height n
+size n@Node{} = _size n
 
-left' :: Tree a -> Tree a
-left' Leaf     = Leaf
-left' n@Node{} = left n
+left :: Tree a -> Tree a
+left Leaf     = Leaf
+left n@Node{} = _left n
 
-right' :: Tree a -> Tree a
-right' Leaf     = Leaf
-right' n@Node{} = right n
+right :: Tree a -> Tree a
+right Leaf     = Leaf
+right n@Node{} = _right n
 
 rotateLeft :: Tree a -> Tree a
 rotateLeft Leaf                    = Leaf
@@ -68,30 +68,36 @@ rotateRight (Node k v (Node xk xv xl xr _ _) r n c) =
 
 flipColor :: Tree a -> Tree a
 flipColor (Node k v l@Node{} r@Node{} n _) =
-    Node k v (setColor l Black) (setColor r Black) n Red
+    Node k v (setColor Black l) (setColor Black r) n Red
 flipColor n = n
 
 put :: Int -> a -> Tree a -> Tree a
 put k v Leaf = Node k v Leaf Leaf 1 Red
-put k v h@Node{} =
-    make h & sat sa rotateLeft & sat sb rotateRight & sat sc flipColor & resize
+put k v (Node nk nv l r n c) =
+    make & sat sa rotateLeft & sat sb rotateRight & sat sc flipColor & resize
   where
-    make Leaf = Leaf
-    make (Node nk nv l r n c) | k < nk    = Node nk nv (put k v l) r n c
-                              | k > nk    = Node nk nv l (put k v r) n c
-                              | otherwise = Node k v l r n c
-    sa x = isRed (right' x) && (not . isRed . left') x
-    sb x = isRed (left' x) && (isRed . left' . left') x
-    sc x = isRed (left' x) && isRed (right' x)
-    resize Leaf                 = Leaf
-    resize (Node rk rv l r _ c) = Node rk rv l r (size l + size r + 1) c
+    make | k < nk    = Node nk nv (put k v l) r n c
+         | k > nk    = Node nk nv l (put k v r) n c
+         | otherwise = Node k v l r n c
+    sa x = isRed (right x) && (not . isRed . left) x
+    sb x = isRed (left x) && (isRed . left . left) x
+    sc x = isRed (left x) && isRed (right x)
+    resize Leaf = Leaf
+    resize (Node rk rv rl rr _ rc) =
+        Node rk rv rl rr (size rl + size rr + 1) rc
 
 delete :: Int -> Tree a -> Tree a
 delete _ Leaf = Leaf
 delete _ _    = undefined
 
+search :: Int -> Tree a -> Maybe a
+search _ Leaf = Nothing
+search key (Node k v l r _ _) | key < k   = search key l
+                              | key > k   = search key r
+                              | otherwise = Just v
+
 putRoot :: Int -> a -> Tree a -> Tree a
-putRoot k v t = flip setColor Black $ put k v t
+putRoot k v t = put k v t & setColor Black
 
 sample :: Tree Char
 sample =
