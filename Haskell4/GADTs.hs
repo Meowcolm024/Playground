dab31dc98e1fb163c873a335d6fb5fef8b1fd59c
Meowcolm024 <luoyicong1026@live.com>
2021-01-14 20:15:08 +0800

    hihi

--- Haskell4/GADTs.hs
+++ Haskell4/GADTs.hs
@@ -1,11 +1,11 @@
 {-# LANGUAGE GADTs #-}
 
 data Expr a where
-    I :: Int -> Expr Int
-    B :: Bool -> Expr Bool
-    Add :: Expr Int -> Expr Int -> Expr Int
-    Mul :: Expr Int -> Expr Int -> Expr Int
-    Eqv  :: Eq a => Expr a -> Expr a -> Expr Bool
+  I :: Int -> Expr Int
+  B :: Bool -> Expr Bool
+  Add :: Expr Int -> Expr Int -> Expr Int
+  Mul :: Expr Int -> Expr Int -> Expr Int
+  Eqv :: Eq a => Expr a -> Expr a -> Expr Bool
 
 eval :: Expr a -> a
 eval (I n) = n
@@ -14,3 +14,13 @@ eval (Add e1 e2) = eval e1 + eval e2
 eval (Mul e1 e2) = eval e1 * eval e2
 eval (Eqv e1 e2) = eval e1 == eval e2
 
+data Empty
+
+data NonEmpty
+
+data SafeList a b where
+  Nil :: SafeList a Empty
+  Cons :: a -> SafeList a b -> SafeList a NonEmpty
+
+safeHead :: SafeList a NonEmpty -> a
+safeHead (Cons x _) = x
---
+++ flaskSample/server.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def form():
+    return render_template('form.html')
+
+@app.route('/hello', methods=['GET', 'POST'])
+def hello():
+    file = open('output.txt', 'a')
+    file.write(f"{request.form['say']} {request.form['to']}\n")
+    file.close()
+    return render_template('greeting.html', say=request.form['say'], to=request.form['to'])
+
+if __name__ == "__main__":
+    app.run()
