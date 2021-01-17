e9c9e039e49134433748b5d853aef6239fb07b1f
Meowcolm024 <luoyicong1026@live.com>
2021-01-17 21:06:24 +0800

    h

--- Python/emmm.py
+++ Python/emmm.py
@@ -1,8 +1,20 @@
-mylist = []
+a = 7
+b = 13
+c = 29
+d = [1, 2, 3]
+e = "hello"
+f = 0
 
-def add():
-    mylist.append(1)
+def omg(c, z, w):
+    global a
+    a += 10
+    b = 3
+    c *= 2
+    z += z
+    w*2
+    f = 233
+    return b
 
-for i in range(5):
-    print(mylist)
-    add()
+f = omg(c, d, e)
+
+print(a, b, c, d, e, f)
