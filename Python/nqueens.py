65ca89f3e901c84901dab7d53a142ebf6f59a655
Meowcolm024 <luoyicong1026@live.com>
2021-02-09 22:34:28 +0800

    n-queens

--- Python/nqueens.py
+++ Python/nqueens.py
@@ -1,7 +1,11 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
 def nqueens(n):
     def helper(qs, r):
         u = len(qs)
-        if u == n: 
+        if u == n:
             return qs
         row = [True for i in range(n)]
         for i in qs:
@@ -18,17 +22,23 @@ def nqueens(n):
         return []
     return helper([], 0)
 
-def print_chess(qs):
-    size = len(qs)
-    def print_pos(n):
-        tmp = [' ' for i in range(size)]
-        tmp[n] = '*'
-        return '|'.join(tmp)
-    print(''.join(['-' for i in range(2*size+1)]))
-    for q in qs:
-        print('|' + print_pos(q) + '|')
-        print(''.join(['-' for i in range(2*size+1)]))
 
-result = nqueens(8)
-print(result)
-print_chess(result)
+# display
+board = np.zeros((8, 8, 3))
+board += 0.5  # "Black" color. Can also be a sequence of r,g,b with values 0-1.
+board[::2, ::2] = 1  # "White" color
+board[1::2, 1::2] = 1  # "White" color
+
+positions = reversed(nqueens(8))
+
+fig, ax = plt.subplots()
+ax.imshow(board, interpolation='nearest')
+
+for y, x in enumerate(positions):
+    # Use "family='font name'" to change the font
+    ax.text(x, y, u'\u2655', size=30, ha='center', va='center')
+
+ax.set(xticks=[], yticks=[])
+ax.axis('image')
+
+plt.show()
