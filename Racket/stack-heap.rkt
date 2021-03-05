8c416aafe729361af2c9ce0adf6155b8f3387f75
Meowcolm024 <luoyicong1026@live.com>
2021-03-05 15:41:25 +0800

    stack and heap

---
+++ Racket/stack-heap.rkt
@@ -0,0 +1,78 @@
+#lang racket
+
+(define (from-to f t) (if (= f t) '() (cons f (from-to (+ f 1) t))))
+
+(define stack '())
+(define (push val) (begin (set! stack (cons val stack)) (- (length stack) 1)))
+(define (pop) (let ((tmp (car stack))) (begin (set! stack (cdr stack)) tmp)))
+(define (stack-ref ptr) (list-ref (reverse stack) ptr))
+
+(define heap (vector #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f)) ;; heap size 16
+(define (halloc size)
+  (define (find-empty pos)
+    (if (vector-ref heap pos)
+      (find-empty (+ pos 1))
+      (if (andmap (lambda (x) (eq? x #f)) (take (list-tail (vector->list heap) pos) size))
+        (begin (for-each (lambda (p) (vector-set! heap p #t)) (from-to pos (+ pos size))) pos)
+        (find-empty (+ pos 1)))))
+  (find-empty 0)) ;; return heap pointer
+(define (heap-ref ptr) (vector-ref heap ptr))
+(define (hfree ptr size)
+  (for-each (lambda (p) (vector-set! heap p #f)) (from-to ptr (+ ptr size))))
+
+(define (auto-var content size)
+  (define ptr (last (map (lambda (x) (push content)) (from-to 0 size))))
+  (define sz size)
+  (define status #t)
+  (lambda (msg) 
+    (if (eq? status #t) 
+      (cond ((eq? msg 'expire) 
+               (begin 
+                 (for-each (lambda (x) (pop)) (from-to 0 sz))
+                 (set! status #f)))
+             ((eq? msg 'content) (stack-ref ptr))
+             ((eq? msg 'tag) 'auto)
+             ((eq? msg 'ptr) ptr)
+             (else '()))
+      'expired!)))
+
+(define (dyn-var ptr size content)
+  (define pointer ptr)
+  (define memory (begin 
+                   (for-each (lambda (p) (vector-set! heap p content)) (from-to ptr (+ ptr size)))
+                   (from-to ptr (+ ptr size))))
+  (define sz size)
+  (define status #t)
+  (lambda (msg) 
+    (if (eq? status #t)
+      (cond ((eq? msg 'content) (map (lambda (x) (vector-ref heap x)) memory))
+            ((eq? msg 'expire) (hfree pointer sz))
+            ((eq? msg 'tag) 'dyn)
+            ((eq? msg 'ptr) pointer)
+            (else 'expired!))
+    '())))
+
+(define (write-heap ptr size content) 
+  (for-each (lambda (p) (vector-set! heap p content)) (from-to ptr (+ ptr size))))
+
+
+;; simulate a scope (auto delete auto variables when out of scope)
+;; v :: variable lsit
+(define (scope v)
+  (define vars v)
+  (lambda (msg)
+    (cond ((eq? msg 'expire)
+            (for-each (lambda (x) (if (eq? 'auto (x 'tag)) (x 'expire) '())) vars))
+          ((eq? msg 'vars) (map (lambda (x) (x 'content)) vars))
+          (else '()))))
+
+
+;; testing vars
+(define a-ptr (halloc 2))
+(define b-ptr (halloc 4))
+(define x (auto-var 'hello 3))
+(define y (auto-var 65 1))
+(define a (dyn-var a-ptr 2 233))
+(define b (dyn-var b-ptr 4 'hi))
+(define test-scope (scope (list a x y b)))
+
