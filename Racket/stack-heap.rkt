cfbe66f71436429476c1f2819d7910f4763f7d13
Meowcolm024 <luoyicong1026@live.com>
2021-03-05 19:51:24 +0800

    update

--- Racket/stack-heap.rkt
+++ Racket/stack-heap.rkt
@@ -1,27 +1,56 @@
 #lang racket
-
+;; helper function
 (define (from-to f t) (if (= f t) '() (cons f (from-to (+ f 1) t))))
 
-(define stack '())
-(define (push val) (begin (set! stack (cons val stack)) (- (length stack) 1)))
-(define (pop) (let ((tmp (car stack))) (begin (set! stack (cdr stack)) tmp)))
-(define (stack-ref ptr) (list-ref (reverse stack) ptr))
+(define (make-stack)
+  (define stack (vector #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f)) ;; stack size 16
+  (define pointer 0)
+  (lambda (msg)
+    (cond ((eq? msg 'push) 
+            (lambda (x) 
+              (begin (vector-set! stack pointer x) (set! pointer (+ pointer 1)) (- pointer 1))))
+          ((eq? msg 'pop)
+            (begin 
+              (set! pointer (- pointer 1))
+              (define top (vector-ref stack pointer))
+              (vector-set! stack pointer #f)
+              top))
+          ((eq? msg 'ref) (lambda (p) (vector-ref stack p)))
+          ((eq? msg 'write) (lambda (p v) (vector-set! stack p v)))
+          ((eq? msg 'show) stack)
+          (else (error "unknown msg")))))
+;; push var to stack
+(define (push val) ((stack 'push) val))
+;; pop stack
+(define (pop) (stack 'pop))
+;; reference
+(define (stack-ref ptr) ((stack 'ref) ptr))
+;; write stack
+(define (stack-write ptr size val) 
+  (for-each (lambda (p) ((stack 'write) p (list-ref val p))) (from-to ptr (+ ptr size))))
+;; new stack
+(define stack (make-stack))
 
+;; new heap
 (define heap (vector #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f #f)) ;; heap size 16
+;; allocate memery in heap
 (define (halloc size)
-  (define (find-empty pos)
+  (define (find-empty pos)  ;; find available memerory
     (if (vector-ref heap pos)
       (find-empty (+ pos 1))
       (if (andmap (lambda (x) (eq? x #f)) (take (list-tail (vector->list heap) pos) size))
         (begin (for-each (lambda (p) (vector-set! heap p #t)) (from-to pos (+ pos size))) pos)
         (find-empty (+ pos 1)))))
-  (find-empty 0)) ;; return heap pointer
+  (find-empty 0)) ;; return the pointer to heap
+;; reference
 (define (heap-ref ptr) (vector-ref heap ptr))
+;; free memory
 (define (hfree ptr size)
   (for-each (lambda (p) (vector-set! heap p #f)) (from-to ptr (+ ptr size))))
 
-(define (auto-var content size)
-  (define ptr (last (map (lambda (x) (push (list-ref content x))) (from-to 0 size))))
+;; create auto variable (stack)
+(define (auto-var size content)
+  (define ptr (car (map (lambda (x) (push (list-ref content x))) (from-to 0 size))))
   (define sz size)
   (define status #t)
   (lambda (msg) 
@@ -31,12 +60,13 @@
                  (for-each (lambda (x) (pop)) (from-to 0 sz))
                  (set! status #f)))
              ((eq? msg 'content) (stack-ref ptr))
-             ((eq? msg 'contents) (map (lambda (x) (stack-ref x)) (from-to ptr sz)))
+             ((eq? msg 'contents) (map stack-ref (from-to ptr (+ ptr sz))))
              ((eq? msg 'tag) 'auto)
              ((eq? msg 'ptr) ptr)
              (else (error "unknown msg")))
       'expired!)))
 
+;; create dynamic variable (heap)
 (define (dyn-var ptr size content)
   (define pointer ptr)
   (define memory (begin 
@@ -49,55 +79,54 @@
   (lambda (msg) 
     (if (eq? status #t)
       (cond ((eq? msg 'content) (heap-ref ptr))
-            ((eq? msg 'contents) (map (lambda (x) (heap-ref x)) memory))
+            ((eq? msg 'contents) (map heap-ref memory))
             ((eq? msg 'expire) (hfree pointer sz))
             ((eq? msg 'tag) 'dyn)
             ((eq? msg 'ptr) pointer)
             (else (error "unknown msg")))
     'expired!)))
 
-(define (write-heap ptr size content) 
+;; write heap via pointer
+(define (heap-write ptr size content) 
   (for-each 
     (lambda (p) 
       (if (eq? (vector-ref heap p) #f)
         (error "not allocated memory!")
         (vector-set! heap p (list-ref content p)))) (from-to ptr (+ ptr size))))
-
-(define (read-heap ptr size) 
+;; read heap via pointer
+(define (heap-read size ptr) 
   (take (drop (vector->list heap) ptr) size))
 
-;; simulate a scope (auto delete auto variables when out of scope)
-;; v :: variable lsit
-(define (scope v)
-  (define vars v)
+;; simulate a scope (delete auto variables when out of scope)
+(define (scope var-list)
+  (define vars var-list)
   (lambda (msg)
     (cond ((eq? msg 'expire)
             (for-each (lambda (x) (if (eq? 'auto (x 'tag)) (x 'expire) '())) vars))
           ((eq? msg 'vars) (map (lambda (x) (x 'content)) vars))
+          ((eq? msg 'add) (lambda (n) (set! vars (append vars n))))
           (else (error "unknown msg")))))
+;; append scope
+(define (scope-add s n) ((s 'add) n))
 
 ;; moniter memory
 (define (show-memory)
   (begin
-    (display stack)
-    (newline)
+    (display "memory\n  ")
+    (display (stack 'show))
+    (display "\n  ")
     (display heap)
     (newline)))
 
-;; testing vars
-(define a-ptr (halloc 2))
-(define b-ptr (halloc 4))
-(define x (auto-var '(1 2 3) 3))
-(define y (auto-var '(65) 1))
-(define a (dyn-var a-ptr 2 '(332 233)))
-(define b (dyn-var b-ptr 4 '(hi hi bye bye)))
-(define test-scope (scope (list a x y b)))
+
+;; EXAMPLES BELOW OwO
+
 
 ;; try using heap and stack
 ;; notice dyn var not cleaned up
 (define (add p q)
   ;; intro vars, one auto, the other dynamic
-  (define p1 (auto-var (list p) 1))
+  (define p1 (auto-var 1 (list p)))
   (define q1 (dyn-var (halloc 1) 1 (list q)))
   ;; add variables to current scope
   (define spq (scope (list p1 q1)))
@@ -119,4 +148,58 @@
             (else (error "unknown msg")))
       'destructed)))
 
-;; TODO: linked list example
+;; demo
+(define (example)
+  (begin
+    ;; before loading vars
+    (displayln ":: program started")
+    (show-memory)
+    ;; allocate ptr for var 1 and 2
+    (define ptr1 (halloc 3))
+    (define ptr2 (halloc 4))
+    (define var1 (dyn-var ptr1 3 '(a b c)))
+    (define var2 (dyn-var ptr2 4 '(1 2 3 4)))
+    ;; auto managed var 3 and 4
+    (define var3 (auto-var 1 '("bbb")))
+    (define var4 (auto-var 3 '(6 6 6)))
+    ;; into scope
+    (define eg-scope (scope (list var1 var2 var3 var4)))
+    (show-memory)
+    ;; stack buffer overflow (write)
+    (stack-write (var3 'ptr) 2 '(X D))
+    (display "var3 ")
+    (displayln (var3 'contents))
+    (display "var4 ")
+    (displayln (var4 'contents))
+    (show-memory)
+    ;; heap buffer overflow (write)
+    (heap-write ptr1 5 '(o v e r !))
+    ;; show result
+    (display "var1 ")
+    (displayln (var1 'contents))
+    (display "var2 ")
+    (displayln (var2 'contents))
+    ;; heap buffer overflow (read)
+    (displayln (heap-read 7 ptr1))
+    ;; free ptr1
+    (hfree ptr1 3)
+    (show-memory)
+    ;; allocate new var 5 and 6 without explict pointers
+    (define var5 (dyn-var (halloc 6) 6 '(r a c k e t)))
+    (define var6 (dyn-var (halloc 1) 1 '(233)))
+    ;; add to scope
+    (scope-add eg-scope (list var5 var6))
+    ;; notice where var 6 goes
+    (show-memory)
+    ;; when exit
+    ;; free var 5 (obtion pointer from variable)
+    (hfree (var5 'ptr) 6)
+    ;; this does not clean var 4 and var 6
+    (eg-scope 'expire)
+    (show-memory)
+    (displayln ":: program ended")
+  )
+)
+
+;; main
+(example)
